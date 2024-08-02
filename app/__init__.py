220fc69ef8e79cca4459c5077b35ddd7721297c6
omar <omartra069@gmail.com>
2024-08-02 16:12:07 +0200

    email sending function

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+import yagmail
 
 app = Flask(__name__)
 
+email_user = "omartra069@gmail.com"
+email_password = "vumv wpwy rzbk ipyz"
+app.secret_key = 'siqwmdlso'
+yag = yagmail.SMTP(email_user, email_password)
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,30 @@
-from flask import render_template, request, url_for
-from app import app
+from flask import render_template, request, url_for, flash, redirect
+from app import app, yag
+import traceback
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        
+        if not name or not email or not message:
+            flash('All fields are required!', 'error')
+            return redirect(url_for('index'))
+
+        subject = 'Contact Form Submission'
+        body = f'Name: {name}\nEmail: {email}\nMessage:\n{message}'
+        try:
+            email_user = "omartra069@gmail.com"
+            yag.send(to=email_user, subject=subject, contents=body)
+            flash('Email sent successfully')
+            print('Email sent successfully')
+            return redirect(url_for('index'))
+        except Exception as e:
+            traceback.print_exc()
+            flash('Failed to send email')
+        
+    
     return render_template('index.html')
 
